63243ed9608444d3b03d481c4b6689be21e1b1d3
Sankluj <matthieu.kluj@gmail.com>
2018-05-13 21:29:37 +0200

    feat(card-availability): support multiple cards found

--- db/2-init-data.sql
+++ db/2-init-data.sql
@@ -5,6 +5,7 @@ INSERT INTO MAGICIANS (name) VALUES
 
 INSERT INTO CARDS (set_code, number, name) VALUES
   ('DOM', 207, 'Teferi, Hero of Dominaria'),
+  ('DOM', 200, 'Oath of Teferi'),
   ('DOM', 132, 'Jaya Ballard'),
   ('XLN', 22, 'Legion''s Landing');
 
@@ -12,4 +13,5 @@ INSERT INTO CARDS_TO_MAGICIANS (magician_did, card_did, amount) VALUES
   (1, 1, 3),
   (1, 2, 1),
   (2, 1, 1),
+  (2, 4, 4),
   (3, 3, 1);
--- micro-services/availability/src/index.js
+++ micro-services/availability/src/index.js
@@ -1,19 +1,19 @@
 const pool = require('./db/index.js');
 
-const getCardIdByCardName = async cardName => {
-  const { rows: [ { did: cardId } = { } ] } = await pool.query(`
+const getCardIdsByCardName = async cardName => {
+  const { rows = [] } = await pool.query(`
     SELECT DID
     FROM CARDS
-    WHERE CARDS.NAME LIKE $1
-  `, [ `%${cardName}%` ]);
+    WHERE LOWER(CARDS.NAME) LIKE $1
+  `, [ `%${cardName.toLowerCase()}%` ]);
 
-  return cardId;
+  return rows.map(({ did }) => did);
 };
 
 const getAvailabilityByCardName = async cardName => {
-  const cardId = await getCardIdByCardName(cardName);
+  const cardIds = await getCardIdsByCardName(cardName);
 
-  if (!cardId) {
+  if (cardIds.length <= 0) {
     throw {
       errorCode: 404,
       errorMessage: 'No cards found with this name'
@@ -23,15 +23,39 @@ const getAvailabilityByCardName = async cardName => {
   const { rows } = await pool.query(`
     SELECT
       MAGICIANS.NAME AS MAGICIAN_NAME,
+      CARDS.DID AS CARD_ID,
       CARDS.NAME AS CARD_NAME,
       CARDS_TO_MAGICIANS.AMOUNT
     FROM CARDS_TO_MAGICIANS
       INNER JOIN CARDS ON CARDS.DID = CARDS_TO_MAGICIANS.CARD_DID
       INNER JOIN MAGICIANS ON MAGICIANS.DID = CARDS_TO_MAGICIANS.MAGICIAN_DID
-    WHERE CARDS.DID = $1
-  `, [ cardId ]);
+    WHERE CARDS.DID IN (${cardIds.join(',')})
 
-  return rows;
+  `);
+
+  const availability = rows.reduce((acc, {
+    magician_name: magicianName,
+    card_id: cardId,
+    card_name: cardName,
+    amount,
+  }) => {
+    const index = acc.findIndex(({ cardId: id }) => cardId === id);
+
+    return index > -1 ?
+      (acc[index].amount[magicianName] = amount) && acc :
+      [
+        ...acc,
+        {
+          cardId,
+          cardName,
+          amount: {
+            [magicianName]: amount
+          }
+        }
+      ];
+  }, []);
+
+  return availability;
 };
 
 module.exports = {
