db99e095317c69c7da1a641c7e847a6485da73cb
Sankluj <matthieu.kluj@gmail.com>
2018-05-13 18:32:29 +0200

    feat(db): hydrate db

--- db/1-init.sql
+++ db/1-init.sql
@@ -1,4 +1,18 @@
 CREATE TABLE MAGICIANS (
-  did         integer PRIMARY KEY,
-  name        varchar(40) NOT NULL CHECK (name <> '')
+  did           SERIAL PRIMARY KEY,
+  name          varchar(40) NOT NULL CHECK (name <> '')
+);
+
+CREATE TABLE CARDS (
+  did           SERIAL PRIMARY KEY,
+  set_code      varchar(40) NOT NULL CHECK (set_code <> ''),
+  number        integer,
+  name          varchar(40) NOT NULL CHECK (name <> '')
+);
+
+CREATE TABLE CARDS_TO_MAGICIANS (
+  magician_did  integer REFERENCES MAGICIANS (did),
+  card_did      integer REFERENCES CARDS (did),
+  amount        integer,
+  PRIMARY KEY (magician_did, card_did)
 );
---
+++ db/2-init-data.sql
@@ -0,0 +1,15 @@
+INSERT INTO MAGICIANS (name) VALUES
+  ('Matthieu'),
+  ('Adrien'),
+  ('Romeo');
+
+INSERT INTO CARDS (set_code, number, name) VALUES
+  ('DOM', 207, 'Teferi, Hero of Dominaria'),
+  ('DOM', 132, 'Jaya Ballard'),
+  ('XLN', 22, 'Legion''s Landing');
+
+INSERT INTO CARDS_TO_MAGICIANS (magician_did, card_did, amount) VALUES
+  (1, 1, 3),
+  (1, 2, 1),
+  (2, 1, 1),
+  (3, 3, 1);
